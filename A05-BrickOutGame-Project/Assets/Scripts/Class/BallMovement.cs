f90663877a56daedb52efd4f243d8b3045b16db1
pringles369 <shinja407@gmail.com>
2024-05-17 16:37:32 +0900

    [Update] Ball Bounce
    공 튀기는 모션 구현

--- A05-BrickOutGame-Project/Assets/Scripts/Class/BallMovement.cs
+++ A05-BrickOutGame-Project/Assets/Scripts/Class/BallMovement.cs
@@ -2,29 +2,60 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
 public class BallMovement : MonoBehaviour
 {
     public float speed;
-    public Rigidbody2D rb2d;
-    public CircleCollider2D cc2d;
+    private Rigidbody2D rb2d;
+    private CircleCollider2D cc2d;
+    private GameController controller;
+    private Vector2 ballVector;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        controller = GetComponent<GameController>();
+        cc2d = GetComponent<CircleCollider2D>();
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb2d.velocity = Vector2.down * 10;
+        controller.OnAimEvent += AimMovementPoint;
+    }
+
+    private void FixedUpdate()
+    {
+        AimMovementPoint(ballVector);
+    }
+
+    private void AimMovementPoint(Vector2 vector)
+    {
+        ballVector = vector;
+        BallMove(vector);
+    }
+
+    private void BallMove(Vector2 vector)
+    {
+        ballVector = vector * 5;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.GetComponent<Rigidbody2D>().velocity = ballVector;
     }
 
     
 
+
+
+
+
     // Update is called once per frame
     void Update()
     {
-        Bounce();
-    }
 
-    private void Bounce()
-    {
-        
     }
 }
