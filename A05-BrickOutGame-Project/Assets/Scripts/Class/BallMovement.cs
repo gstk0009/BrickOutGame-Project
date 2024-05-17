c89f4bf80983ea89edb37bafcc8c7622b65d04d2
pringles369 <shinja407@gmail.com>
2024-05-17 12:49:27 +0900

    Update BallMovement.cs

--- A05-BrickOutGame-Project/Assets/Scripts/Class/BallMovement.cs
+++ A05-BrickOutGame-Project/Assets/Scripts/Class/BallMovement.cs
@@ -1,17 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
+    public float speed;
+    public Rigidbody2D rb2d;
+    public CircleCollider2D cc2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
+
+    
 
     // Update is called once per frame
     void Update()
+    {
+        Bounce();
+    }
+
+    private void Bounce()
     {
         
     }
