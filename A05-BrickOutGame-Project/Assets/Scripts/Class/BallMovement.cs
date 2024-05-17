1c69de37cd04d720f89b36c745ed87efae1c24bc
gstk0009 <gstk0009@naver.com>
2024-05-17 17:23:09 +0900

    [FIX] Ball, Paddle
    1. 볼 움직임 수정
    2. Paddle 충돌 구현

--- A05-BrickOutGame-Project/Assets/Scripts/Class/BallMovement.cs
+++ A05-BrickOutGame-Project/Assets/Scripts/Class/BallMovement.cs
@@ -6,56 +6,17 @@ using UnityEngine.InputSystem.XR;
 
 public class BallMovement : MonoBehaviour
 {
-    public float speed;
+    public float speed = 10f;
     private Rigidbody2D rb2d;
-    private CircleCollider2D cc2d;
-    private GameController controller;
-    private Vector2 ballVector;
 
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        controller = GetComponent<GameController>();
-        cc2d = GetComponent<CircleCollider2D>();
     }
 
     // Start is called before the first frame update
     private void Start()
     {
-        rb2d.velocity = Vector2.down * 10;
-        controller.OnAimEvent += AimMovementPoint;
-    }
-
-    private void FixedUpdate()
-    {
-        AimMovementPoint(ballVector);
-    }
-
-    private void AimMovementPoint(Vector2 vector)
-    {
-        ballVector = vector;
-        BallMove(vector);
-    }
-
-    private void BallMove(Vector2 vector)
-    {
-        ballVector = vector * 5;
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        collision.GetComponent<Rigidbody2D>().velocity = ballVector;
-    }
-
-    
-
-
-
-
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        rb2d.velocity = Vector2.down * speed;
     }
 }
